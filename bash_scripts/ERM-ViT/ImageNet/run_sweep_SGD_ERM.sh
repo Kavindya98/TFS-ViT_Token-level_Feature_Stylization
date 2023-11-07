adc03aff048607029dd1e891c43622b68c0eacb7
Kavindya98 <kpimbulgoda@gmail.com>
2023-11-07 17:42:04 +1100

    ImageNet and PACS scripts

--- bash_scripts/ERM-ViT/ImageNet/run_sweep_SGD_ERM.sh
+++ bash_scripts/ERM-ViT/ImageNet/run_sweep_SGD_ERM.sh
@@ -3,11 +3,11 @@
 algorithms=RandConv_CNN
 datasets=ImageNet # PACS, VLCS, OfficeHome, TerraIncognita, DomainNet, ImageNet_9 
 data_dir=/media/SSD2/Dataset 
-output_dir=./Results/${datasets}/RandConv_CNN_2
+output_dir=./Results/${datasets}/RandConv_CNN_C
 for command in delete_incomplete launch
 do
     python -m domainbed.scripts.sweep ${command} --data_dir=${data_dir} \
     --output_dir=${output_dir}  --command_launcher multi_gpu --algorithms ${algorithms}  \
-    --single_domain_gen  --datasets ${datasets}  --n_hparams 1 --n_trials 3  \
+    --single_domain_gen  --datasets ${datasets}  --n_hparams 1 --n_trials 2  \
     --hparams """{\"batch_size\":32,\"lr\":0.0001,\"resnet_dropout\":0.0,\"weight_decay\":0.0,\"custom_train_val\":true,\"custom_train\":0,\"custom_val\":1,\"resnet18\":false,\"fixed_featurizer\":false}"""
 done
