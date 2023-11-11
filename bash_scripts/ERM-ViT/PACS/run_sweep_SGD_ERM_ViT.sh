bc4021b7fa0075757e1a10513aedad0d64867c4e
Kavindya98 <kpimbulgoda@gmail.com>
2023-11-12 01:12:32 +1100

    run commands

--- bash_scripts/ERM-ViT/PACS/run_sweep_SGD_ERM_ViT.sh
+++ bash_scripts/ERM-ViT/PACS/run_sweep_SGD_ERM_ViT.sh
@@ -12,8 +12,8 @@ do
     do 
         output_dir=./Results/${datasets}/ERM_ViT/${backbone}
         python -m domainbed.scripts.sweep ${command} --data_dir=${data_dir} \
-        --output_dir=${output_dir}  --command_launcher gpu_1 --algorithms ${algorithms}  \
-        --single_domain_gen  --datasets ${datasets}  --n_hparams 1 --n_trials 2  \
-        --hparams """{\"backbone\":\"${backbone}\",\"batch_size\":32,\"lr\":5e-05 ,\"resnet_dropout\":0.0,\"weight_decay\":0.0,\"fixed_featurizer\":false}"""
+        --output_dir=${output_dir}  --command_launcher gpu_3 --algorithms ${algorithms}  \
+        --single_domain_gen  --datasets ${datasets}  --n_hparams 1 --n_trials 1  \
+        --hparams """{\"backbone\":\"${backbone}\",\"batch_size\":32,\"lr\":5e-05 ,\"resnet_dropout\":0.0,\"weight_decay\":0.0,\"fixed_featurizer\":false,\"empty_head\":true}"""
     done
 done
--- bash_scripts/ERM-ViT/PACS/run_sweep_SGD_Randconv_ViT.sh
+++ bash_scripts/ERM-ViT/PACS/run_sweep_SGD_Randconv_ViT.sh
@@ -13,7 +13,7 @@ do
         output_dir=./Results/${datasets}/RandConv_ViT/${backbone}
         python -m domainbed.scripts.sweep ${command} --data_dir=${data_dir} \
         --output_dir=${output_dir}  --command_launcher multi_gpu --algorithms ${algorithms}  \
-        --single_domain_gen  --datasets ${datasets}  --n_hparams 1 --n_trials 2  \
-        --hparams """{\"backbone\":\"${backbone}\",\"batch_size\":32,\"lr\":5e-05 ,\"resnet_dropout\":0.0,\"weight_decay\":0.0,\"fixed_featurizer\":false}"""
+        --single_domain_gen  --datasets ${datasets}  --n_hparams 1 --n_trials 1  \
+        --hparams """{\"backbone\":\"${backbone}\",\"batch_size\":32,\"lr\":5e-05 ,\"resnet_dropout\":0.0,\"weight_decay\":0.0,\"fixed_featurizer\":false,\"empty_head\":true}"""
     done
 done
